78431e56f230ae033357acf41e2445ee60fc6dc0
namalleng <nyu0kim@gmail.com>
2024-11-27 23:54:37 +0900

    회원 가입 구현하기 : 서비스 메서드 코드 작성하기

---
+++ src/main/java/me/nyungnim/blog/service/UserService.java
@@ -0,0 +1,27 @@
+package me.nyungnim.blog.service;
+
+import lombok.RequiredArgsConstructor;
+import me.nyungnim.blog.domain.User;
+import me.nyungnim.blog.domain.repository.UserRepository;
+import me.nyungnim.blog.dto.AddUserRequest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // DTO를 받아 새로운 사용자를 저장, 저장한 고유ID를 반환하는 메서드
+    public Long save(AddUserRequest dto) {
+        // JPA의 save 메서드를 호출해 User 엔티티를 데이터베이스에 저장
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                // 패스워드 암호화, 패스워드를 저장할 때 시큐리티를 설정하며 패스워드 인코딩용으로 등록한 빈을 사용해서 암호화한 후에 저장
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
