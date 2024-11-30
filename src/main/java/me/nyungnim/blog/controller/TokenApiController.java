7dc76f955951bfdf962c495563679e9a34be355c
namalleng <nyu0kim@gmail.com>
2024-11-30 14:39:11 +0900

    토큰 API 구현 : 토큰 컨트롤러 추가하기 + 테스트 코드 작성 - 리프레시 토큰을 전달받아 검증하고 유효한 리프레시 토큰이라면 새로운 액세스 토큰을 생성하는 토큰 API 구현, 그 중 컨트롤러에 해당 - 테스트 코드 추가

---
+++ src/main/java/me/nyungnim/blog/controller/TokenApiController.java
@@ -0,0 +1,27 @@
+package me.nyungnim.blog.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import me.nyungnim.blog.dto.CreateAccessTokenRequest;
+import me.nyungnim.blog.dto.CreateAccessTokenResponse;
+import me.nyungnim.blog.service.TokenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken
+            (@RequestBody CreateAccessTokenRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
