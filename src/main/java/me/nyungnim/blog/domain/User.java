26add1dae6d1c9f32f9307aab0ee3ebd1b39d8f6
namalleng <nyu0kim@gmail.com>
2024-12-01 05:27:46 +0900

    OAuth2 서비스 구현하기 : 도메인 user에 사용자 이름 추가 + 사용자 정보 서비스 OAuth 패키지에 추가 - 리소스 서버에서 보내주는 사용자 정보를 불러오는 메서드를 통해 사용자 조회 - user 테이블에 사용자 정보가 있다면 이름을 업데이트, 없다면 user 테이블에 회원 데이터 추가

--- src/main/java/me/nyungnim/blog/domain/User.java
+++ src/main/java/me/nyungnim/blog/domain/User.java
@@ -32,11 +32,17 @@ public class User implements UserDetails {  // UserDetails를 상속받아 인�
     @Column(name = "password")
     private String password;
 
+    // 사용자 이름
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
     // 이메일과 비밀번호를 입력받아 사용자 객체 생성
+    // 생성자에 nickname 추가
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
     }
 
     @Override // 사용자 권한 반환 -> 단일 권한 "user" 반환
@@ -87,5 +93,12 @@ public class User implements UserDetails {  // UserDetails를 상속받아 인�
     public boolean isEnabled() {
         // 계정이 사용 가능한지 확인하는 로직
         return true;    // true -> 사용 가능
+    }
+
+    // 사용자 이름 변경
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
     }
 }
